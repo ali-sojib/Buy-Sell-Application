8818a62e6b6e8d324315139b3c47fd299e76cd44
alish <alishojib12@gmail.com>
2022-06-14 01:28:59 +0600

    first commit

---
+++ android/app/src/main/kotlin/skillbuster/org/uk/buy_sell_appliction/MainActivity.kt
@@ -0,0 +1,6 @@
+package skillbuster.org.uk.buy_sell_appliction
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,44 @@
+import 'package:buy_sell_appliction/constants/global_variable.dart';
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Buy sell Application',
+      theme: ThemeData(
+        scaffoldBackgroundColor: GlobalVariables.backgroundColor,
+        colorScheme: ColorScheme.light(
+          primary: GlobalVariables.secondaryColor,
+        ),
+        appBarTheme: AppBarTheme(
+          elevation: 0,
+          iconTheme: IconThemeData(color: Colors.black),
+        ),
+      ),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Text('Buy & Sell'),
+        ),
+        body: Column(
+          children: [
+            Center(
+              child: const Text('Flutter Buy Sell Application'),
+            ),
+            ElevatedButton(
+              onPressed: () {},
+              child: Text("click"),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
