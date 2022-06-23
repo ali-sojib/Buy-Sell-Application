2636f29716c7438b273f958fdd4f071c478074e7
alish <alishojib12@gmail.com>
2022-06-24 00:36:50 +0600

    auth screen half way

--- lib/features/screens/auth_screen.dart
+++ lib/features/screens/auth_screen.dart
@@ -1,7 +1,11 @@
-import 'dart:html';
-
+import 'package:buy_sell_appliction/common/widgets/custom_textfield.dart';
 import 'package:buy_sell_appliction/constants/global_variable.dart';
 import 'package:flutter/material.dart';
+
+enum Auth {
+  singin,
+  singup,
+}
 
 class AuthScreen extends StatefulWidget {
   static const String routeName = '/auth-screen';
@@ -12,6 +16,23 @@ class AuthScreen extends StatefulWidget {
 }
 
 class _AuthScreenState extends State<AuthScreen> {
+  Auth _auth = Auth.singup;
+  final _singUpFormKey = GlobalKey<FormState>();
+  final _singInFormKey = GlobalKey<FormState>();
+
+  final TextEditingController _emailController = TextEditingController();
+  final TextEditingController _passwordController = TextEditingController();
+  final TextEditingController _nameController = TextEditingController();
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    super.dispose();
+    _emailController.dispose();
+    _passwordController.dispose();
+    _nameController.dispose();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -32,11 +53,57 @@ class _AuthScreenState extends State<AuthScreen> {
                   fontWeight: FontWeight.bold,
                 ),
               ),
-              // leading: Radio(
-              //     activeColor: GlobalVariables.secondaryColor,
-              //     value: value,
-              //     groupValue: groupValue,
-              //     onChanged: onChanged),
+              leading: Radio(
+                activeColor: GlobalVariables.secondaryColor,
+                value: Auth.singup,
+                groupValue: _auth,
+                onChanged: (Auth? val) {
+                  setState(() {
+                    _auth = val!;
+                  });
+                },
+              ),
+            ),
+            if (_auth == Auth.singup)
+              Container(
+                padding: const EdgeInsets.all(8),
+                child: Form(
+                  key: _singUpFormKey,
+                  child: Column(
+                    children: [
+                      CustomTextField(
+                        controller: _nameController,
+                        hintText: "Name",
+                      ),
+                      CustomTextField(
+                        controller: _emailController,
+                        hintText: "Email",
+                      ),
+                      CustomTextField(
+                        controller: _passwordController,
+                        hintText: "Email",
+                      ),
+                    ],
+                  ),
+                ),
+              ),
+            ListTile(
+              title: const Text(
+                'Sing In',
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              leading: Radio(
+                activeColor: GlobalVariables.secondaryColor,
+                value: Auth.singin,
+                groupValue: _auth,
+                onChanged: (Auth? val) {
+                  setState(() {
+                    _auth = val!;
+                  });
+                },
+              ),
             )
           ],
         ),
