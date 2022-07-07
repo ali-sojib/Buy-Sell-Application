3477e3a9afe79b2242842462dcbadf4fbdd84a62
alish <alishojib12@gmail.com>
2022-07-07 23:08:51 +0600

    connected with mongodb

--- server/index.js
+++ server/index.js
@@ -1,5 +1,6 @@
 //IMPORTS FOMR PACKAGES
 const express = require('express');
+const mongoose = require('mongoose');
 
 //IMPORTS FROM OTER FILES
 const authRouter=require("./routes/auth");
@@ -8,11 +9,20 @@ const authRouter=require("./routes/auth");
 //INIT
 const PORT = 3000;
 const app = express();
-
+const DB = "mongodb+srv://ali_sojib:shokh@cluster0.ljbfg.mongodb.net/?retryWrites=true&w=majority";
 //MIDDLE WARE
 //CLIENT ->{MIDDLE WARE}-> SERVER -> CLIENT ->
 app.use(authRouter);
 
+//Connection
+mongoose
+    .connect(DB)
+    .then(()=>{
+        console.log("connection successful");
+    })
+    .catch((e)=>{
+        console.log(e);
+    });
 
 app.listen(PORT, ()=>{
     console.log(`connected at port ${PORT} HELLO`);
--- server/routes/auth.js
+++ server/routes/auth.js
@@ -2,8 +2,21 @@ const express = require('express');
 
 const authRouter = express.Router();
 
-authRouter.get('/user',(req,res)=>{
-    res.json({msg:"Muhammad Ali Sojib"})
-})
+authRouter.post('/api/signup',(req,res)=>{
+    // console.log(req.body);
+    /*
+    {
+        'name': name,
+        'email': email,
+        'password':password
+        for these value and key we have to declared saparate 
+        varibale but in JS we can just set in const like destructuring
+    }
+    */
+    const {name,email,password} = req.body;
+    //get the data form client
+    //post that data in database
+    //return that data in the user
+});
 
 module.exports= authRouter;
