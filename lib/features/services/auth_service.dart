68932b028aad8c0f11992e2f8852032d3a0829bd
alish <alishojib12@gmail.com>
2022-07-10 23:59:43 +0600

    auth service

--- lib/features/services/auth_service.dart
+++ lib/features/services/auth_service.dart
@@ -1,9 +1,19 @@
+import 'package:buy_sell_appliction/models/user.dart';
+
 class AuthService {
   //sign up user
   void signUpUser(
       {required String eamil,
       required String password,
       required String name}) async {
-    try {} catch (e) {}
+    try {
+      User user = User(
+          id: '',
+          name: name,
+          password: password,
+          address: '',
+          type: '',
+          token: '');
+    } catch (e) {}
   }
 }
