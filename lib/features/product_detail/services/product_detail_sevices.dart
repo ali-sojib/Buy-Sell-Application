3c2865d5ae8db6b20e44711c9ea7a0e580ad89ce
alish <alishojib12@gmail.com>
2022-09-03 23:39:14 +0600

    product detail service dart file created 1

--- lib/features/product_detail/services/product_detail_sevices.dart
+++ lib/features/product_detail/services/product_detail_sevices.dart
@@ -1,12 +1,14 @@
 import 'package:buy_sell_appliction/models/product.dart';
 import 'package:buy_sell_appliction/provirders/user_provider.dart';
-import 'package:flutter/cupertino.dart';
 import 'package:provider/provider.dart';
+import 'package:flutter/material.dart';
 
 class ProductDetailServices {
   void rateProduct({
     required BuildContext context,
     required Product product,
     required double rating,
-  }) async {}
+  }) async {
+    final userProvider = Provider.of<UserProvider>(context, listen: false);
+  }
 }
